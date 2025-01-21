ecebfe3b6b40a45e34da38b74ba1085f11c7e262
Atharv <atharvpathak248@gmail.com>
2025-01-21 08:55:38 +0530

    Build

--- components/leaderboard.tsx
+++ components/leaderboard.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect, useRef } from "react"
+import { useState, useRef } from "react"
 import { cn } from "@/lib/utils"
 import { PLAYERS } from "@/constants"
 import { assignRanks, formatScore } from "@/utils/leaderboard"
